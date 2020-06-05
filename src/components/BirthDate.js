73eeaf4cb8606713df8beb290f2fcaa4624a07b5
Courtney <cfzahner@gmail.com>
2020-06-05 11:57:55 -0500

    Add imports and exports

---
+++ src/components/BirthDate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export class BirthDate extends React.Component {
+  render() {}
+}
---
+++ src/components/Phone.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export class Phone extends React.Component {
+  render() {}
+}
