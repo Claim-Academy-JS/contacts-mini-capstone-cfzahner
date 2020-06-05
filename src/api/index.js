7c3983c3ea455d7d427ed893e74dd02af92d066e
Courtney <cfzahner@gmail.com>
2020-06-05 11:35:43 -0500

    Add api to fetch contacts

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,6 @@
 // TODO: Add API method to retrieve contacts
+export const getContacts = async (contacts) => {
+  const resp = await window.fetch("https://randomuser.me/api/");
+  return resp.json;
+};
+console.log(getContacts);
