c171a59af9b7067cf4017f105210ce0579d0d148
Courtney <cfzahner@gmail.com>
2020-06-09 18:17:48 -0500

    Declare state and properties for icons. Create map to return icon and info

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,17 +1,50 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Pic } from "./Pic";
+import { InfoDisplay } from "./InfoDisplay";
+import { InfoToggle } from "./InfoToggle";
+import { getStateProxy } from "utils";
+
+import "./ContactCard.css";
+import styles from "./Pic.module.css";
 
 export class ContactCard extends React.Component {
   static propTypes = {
     cell: PropTypes.string,
-    dob: PropTypes.object,
     email: PropTypes.string,
-    location: PropTypes.object,
     name: PropTypes.object,
     picture: PropTypes.object,
   };
+
+  state = {
+    activeInfo: "cell",
+  };
+
+  stProxy = getStateProxy(this);
+
+  infoToggles = [
+    {
+      icon: "mobile",
+      info: "cell",
+    },
+    {
+      icon: "envelope",
+      info: "cell",
+    },
+  ];
+  renderInfoToggle() {
+    return this.infoToggles.map(({ icon, info }) => (
+      <InfoToggle key={icon} icon={icon} info={info} stProxy={this.stProxy} />
+    ));
+  }
+
   render() {
-    return <Pic name={this.props.name} pic={this.props.picture} />;
+    return (
+      <div className="ContactCard">
+        <InfoDisplay info={this.props[this.state.activeInfo]} />
+        <Pic name={this.props.name} pic={this.props.picture} />
+        <div className={styles.InfoToggle}>{this.renderInfoToggle()}</div>
+      </div>
+    );
   }
 }
