692f622928ecda0b6bb53351a1b7ac921c292447
Courtney <cfzahner@gmail.com>
2020-06-09 12:44:08 -0500

    Import pic and update render to access picture

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,17 @@
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from "prop-types";
+import React from "react";
+import { Pic } from "./Pic";
 
 export class ContactCard extends React.Component {
-  static propTypes = const string: PropTypes.Requireable<string>
-  cell: PropTypes.string,
-  dob: PropTypes.object,
-  email: PropTypes.string,
-  location: PropTypes.object,
-  name: PropTypes.object,
-  picture: PropTypes.object,
-}
-render() {
-  return <h1>Contact Card!</h1>
+  static propTypes = {
+    cell: PropTypes.string,
+    dob: PropTypes.object,
+    email: PropTypes.string,
+    location: PropTypes.object,
+    name: PropTypes.object,
+    picture: PropTypes.object,
+  };
+  render() {
+    return <Pic name={this.props.name} pic={this.props.picture} />;
+  }
 }
