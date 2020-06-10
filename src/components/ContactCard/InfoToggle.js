b560faad3b9630c32ecaaec768783b78dc35d73b
Courtney <cfzahner@gmail.com>
2020-06-10 14:34:36 -0500

    Update imports, proptypes, handeclick, and render

--- src/components/ContactCard/InfoToggle.js
+++ src/components/ContactCard/InfoToggle.js
@@ -1,1 +1,27 @@
 import "font-awesome/css/font-awesome.min.css";
+import PropTypes from "prop-types";
+import React from "react";
+import "./InfoToggle.css";
+import styles from "./InfoToggle.module.css";
+import "font-awesome/css/font-awesome.min.css";
+
+export class InfoToggle extends React.Component {
+  static propTypes = {
+    icon: PropTypes.string.isRequired,
+    info: PropTypes.string,
+    stProxy: PropTypes.object,
+  };
+
+  handleClick = () => {
+    this.props.stProxy.activeInfo = this.props.info;
+  };
+
+  render() {
+    const className =
+      this.props.stProxy.state.activeInfo === this.props.info
+        ? `InfoToggle fa fa-${this.props.icon} ${styles.active}`
+        : `InfoToggle fa fa-${this.props.icon} `;
+
+    return <span className={className} onClick={this.handleClick} />;
+  }
+}
